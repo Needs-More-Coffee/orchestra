da450b1b800dbd68086be4ebe701f9fd4e51c386
Needs-More-Coffee <tjwood1996@gmail.com>
2026-04-03 10:06:20 -0500

    init: loader skeleton and environment setup

---
+++ core/loader.py
@@ -0,0 +1,9 @@
+class Loader:
+    def __init__(self, base_path):
+        self.base_path = base_path
+    
+    def load_file(self, relative_path):
+        pass
+    
+    def load_voice(self, voice_name):
+        pass
