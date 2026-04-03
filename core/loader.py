0452724c14d70ab32310bb2917cb16404e0dba81
Needs-More-Coffee <tjwood1996@gmail.com>
2026-04-03 10:10:18 -0500

    feat: loader load_file reads local documents

--- core/loader.py
+++ core/loader.py
@@ -1,9 +1,17 @@
+import os
+
 class Loader:
     def __init__(self, base_path):
         self.base_path = base_path
     
     def load_file(self, relative_path):
-        pass
+        full_path = os.path.join(self.base_path, relative_path)
+        with open(full_path, 'r') as f:
+            return f.read()
     
     def load_voice(self, voice_name):
         pass
+
+if __name__ == "__main__":
+    loader = Loader("ai_council")
+    print(loader.load_file("KERNEL.md"))
